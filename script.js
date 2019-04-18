0c40223a453c37b5df69045e038a4c3e987e749c
Lucas Melo <luucasmell@gmail.com>
2019-04-18 10:12:40 -0300

    Criar programa do estado emocional da Ana com dom

---
+++ script.js
@@ -0,0 +1,33 @@
+let emocionalDaAna = document.querySelector("p");
+let titulo = document.querySelector("h1");
+let corpo = document.querySelector("body");
+let imagem = document.querySelector("img");
+let botaoDarChocolate = document.querySelector("#chocolate");
+let botaoEsquecerMateria = document.querySelector("#esquecer");
+
+// TROCAR O HUMOR DA ANA
+function darChocolate(){
+  corpo.style.backgroundColor = "yellow";
+  emocionalDaAna.innerHTML="A Ana fica feliz ao ganhar lindt";
+  imagem.src="https://cdn.shopify.com/s/files/1/1061/1924/products/Shyly_Smiling_Face_Emoji_large.png?v=1480481055";
+
+}
+
+function deixarTriste(){
+  corpo.style.backgroundColor="gray";
+  emocionalDaAna.innerHTML="A Ana ficou triste";
+  imagem.src = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/96/disappointed-but-relieved-face_1f625.png";
+}
+
+botaoDarChocolate.onclick=darChocolate;  
+botaoEsquecerMateria.onclick=deixarTriste;
+
+
+// MANIPULANDO HTML
+titulo.innerHTML = "EMOCIONAL DA ANA!"
+imagem.src = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/96/disappointed-but-relieved-face_1f625.png";
+
+// MANIPULANDO CSS
+titulo.style.color = "blue";
+emocionalDaAna.style.fontSize = "8pt";
+corpo.style.backgroundColor = "gray";
